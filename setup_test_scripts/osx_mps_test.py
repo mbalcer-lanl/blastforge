51187ac12b794720df95001f73fef83bc120a435
hickmank <hickmank@lanl.gov>
2024-12-23 09:47:52 -0700

    Added check for torchRL and gymnasium versions

--- setup_test_scripts/osx_mps_test.py
+++ setup_test_scripts/osx_mps_test.py
@@ -1,7 +1,17 @@
-"""Test MPS torch."""
+"""Test MPS torch.
+
+Also, make sure torchRL and Gymnasium are installed.
+
+"""
 
 import torch
+import torchrl
+import gymnasium
 
+
+print('Torch version:', torch.__version__)
+print('Torch-RL version:', torchrl.__version__)
+print('Gymnasium version:', gymnasium.__version__)
 
 # Check if MPS is available
 if torch.backends.mps.is_available():
